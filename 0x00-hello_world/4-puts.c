75fbe42f2fd1ab8952771b083c9c977f4efe38f3
Chris <you@example.com>
2022-09-15 02:20:01 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\" Programming is like building a multilungal puzzle");
+	puts ("\" Programming is like building a multilingual puzzle");
 	return(0);
 
 }
