c23540c565a546a537cf37674d6cb7a6f34f89a7
Okash <jasonmarika10@gmail.com>
2023-02-21 06:36:09 -0800

    11-print_to

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,36 +8,18 @@
  */
 
 void print_to_98(int n)
-
 {
-	if (n <= 98)
+	while (n < 98)
 	{
-	for (; n <= 98; n++)
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
 	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
+		printf("%i, ", n);
+		n--;
 	}
-	else
-	{
-	printf("%d. ", n);
-	}
-	}
-	}
-	else
-	{
-	for (; n >= 98; n--)
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d, ", n);
-	}
-	}
+
+	printf("98");
+	putchar('\n');
 }
